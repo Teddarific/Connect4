405bfc57a78d555ea6b69e92142c58271bc44492
Teddarific <teddy.ni.19@dartmouth.edu>
2016-08-11 21:53:32 -0400

    Major UI changes

--- app/components/board/boardController.js
+++ app/components/board/boardController.js
@@ -1,10 +1,17 @@
 function boardCtrl(boardService){
     this.tableData = boardService.getTableFormattedData();
+    this.colData = boardService.colData;
     this.playerMove = function(col){
         boardService.playerMove(col);
         this.tableData = boardService.getTableFormattedData();
     }
     this.hoveredCol = -1;
+    this.isGhostPos = function(rowIndex,colIndex){
+         if((7-rowIndex) == this.colData[colIndex].length && colIndex == this.hoveredCol){
+           return true;
+         }
+         return false;
+    }
 }
 
 angular
